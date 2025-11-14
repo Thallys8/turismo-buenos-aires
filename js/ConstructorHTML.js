9e664dee10a2af540d4678f6ce3f2412a0f7941a
Tomas Guardo <39739544+Tomig02@users.noreply.github.com>
2025-11-14 19:33:36 -0300

    nuevos diagramas y modificaciones en el sistema de clases js

--- js/ConstructorHTML.js
+++ js/ConstructorHTML.js
@@ -1,50 +1,14 @@
 import filtroAtracciones from "./FiltroAtracciones.js";
+import TarjetaAtraccion from "./TarjetaAtraccion.js";
 
 class ConstructorHTML {
 
     /**
      * Crea las tarjetas de las atraciones utilizando la informacion almacenada
-     * @param {object} datosAtraccion Datos de la atraccion
-     * @returns {HTMLElement} tarjetaHTML con los datos de atraccion recibidos
+     * @param {object} criterios parametros de las atracciones deseadas
+     * @param {Function} callbackReserva funcion a ejecutarse al hacer click en "Reservar"
+     * @returns {HTMLElement[]} tarjetaHTML con los datos de atraccion recibidos
     */
-    CrearTarjetaAtraccion( datosAtraccion, callbackReserva ){
-    
-        let container = document.createElement("article");
-        container.className = "tarjeta col-6 container";
-
-        container.innerHTML = `
-            <div class="row h-md-100">
-                <hgroup class="col-12">
-                    <h3>${datosAtraccion.titulo}</h3>
-                    <p>${datosAtraccion.subtitulo}</p>
-                </hgroup>
-                <p class="col-12">${datosAtraccion.descripcion}</p>
-
-                <details class="col-12">
-                    <summary>Horarios</summary>
-                    <p>${datosAtraccion.horarios}</p>
-                </details>
-                <label class="direccion-label col-12">Direccion</label> <br>
-                <iframe class="google-maps ratio ratio-16x9 col-12" id="${datosAtraccion.idMapa}"
-                    width="180"
-                    height="150"
-                    style="border:0"
-                    loading="lazy"
-                    allowfullscreen
-                    referrerpolicy="no-referrer-when-downgrade"
-                    src="${datosAtraccion.urlMapa}">
-                </iframe>
-
-                <button type="button" class="btn" value="${datosAtraccion.id}" onclick="${callbackReserva}">Reservar</button>
-            </div>
-
-            <img loading="lazy" src="${datosAtraccion.urlFoto}" alt="${datosAtraccion.altFoto}" >
-        `;
-
-        return container;
-    }
-
-    
     crearAtracciones( criterios, callbackReserva )
     {
         const nuevasTarjetas = [];
@@ -52,7 +16,8 @@ class ConstructorHTML {
             criterios.momento, criterios.horario, criterios.actividad, criterios.grupo
         );
         listaDatos.forEach( atraccion => {
-            nuevasTarjetas.push( this.CrearTarjetaAtraccion(atraccion, callbackReserva) );
+            const tarjeta = new TarjetaAtraccion().crearHTML(atraccion, callbackReserva);
+            nuevasTarjetas.push( tarjeta.getHTML() );
         });
 
         return nuevasTarjetas;
